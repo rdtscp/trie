522ba13da5df5c46da37913a01471291849255f1
Alexander Wilson <acwilson96@gmail.com>
2018-09-09 13:21:51 +0100

    Implementation for TrieNode

--- src/TrieNode.cpp
+++ src/TrieNode.cpp
@@ -4,17 +4,51 @@
 
 #include "../include/Trie.h"
 
-TrieNode::TrieNode(std::string word) {
+TrieNode::TrieNode(std::string word) : children() {
   insert(word);
 }
 
+TrieNode::~TrieNode() {
+  for (const auto& child: children)
+    delete child.second;
+}
+
 bool TrieNode::hasString(std::string word) {
-  /* @TODO */
-  return true;
+  if (word == "")
+    return isEntry;
+
+  char        head = word[0];
+  std::string tail = getTail(word);
+  if (children.find(head) != children.end())
+    return children[head]->hasString(tail);
+
+  return false;
+}
+
+bool TrieNode::hasPrefix(std::string word) {
+  if (word == "")
+    return true;
+
+  char        head = word[0];
+  std::string tail = getTail(word);
+  if (children.find(head) != children.end())
+    return children[head]->hasPrefix(tail);
+
+  return false;
 }
 
 void TrieNode::insert(std::string word) {
-  /* @TODO */
+  if (word == "")
+    isEntry = true;
+  else {
+    char        head = word[0];
+    std::string tail = getTail(word);
+
+    if (children.find(head) == children.end())
+      children[head] = new TrieNode(tail);
+    else
+      children[head]->insert(tail);
+  }
 }
 
 std::string TrieNode::getTail(std::string word) {
