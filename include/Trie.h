f94773f9b370303c4292c4a2d4dd1d83680f7878
Alexander Wilson <acwilson96@gmail.com>
2018-09-09 14:34:00 +0100

    Trie::Trie(), Trie::~Trie(), and Trie::insert(), includes comments.

--- include/Trie.h
+++ include/Trie.h
@@ -8,16 +8,30 @@ class Trie {
 
   public:
 
+    /* Constructs an Empty Trie */
+    Trie();
+
+    /* Takes in a vector of strings, and constructs a Trie. */
     Trie(std::vector<std::string> dictionary);
 
+    /* Cleans up the Trie */
+    ~Trie();
+
+    /* Queries the Trie to see if a string exists. */
     bool hasString(std::string query);
 
+    /* Queries the Trie to see if a prefix of a string exists. */
     bool hasPrefix(std::string prefix);
 
-  private:
+    /* Inserts a word into the Trie. */
+    void insert(std::string word);
 
+  private:
+  
+    /* Contains all the words in the dictionary starting from their first character. */
     std::map<char, TrieNode*> dictionary;
 
+    /* Helper for safely returning the tail of a string. */
     std::string getTail(std::string word);
 
 };
