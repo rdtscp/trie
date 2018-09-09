3112540667f0cd4106b304d20c4bcf1f38afc6b0
Alexander Wilson <acwilson96@gmail.com>
2018-09-09 14:28:57 +0100

    Add comments to header file.

--- include/TrieNode.h
+++ include/TrieNode.h
@@ -6,20 +6,29 @@ class TrieNode {
 
   public:
 
+    /* Takes in a string representing the remaining portion of the word, and creates a structure accordingly. */
     TrieNode(std::string word);
+
+    /* Cleans up the Trie */
     ~TrieNode();
 
+    /* Used recursively to determine if a string exists within the parent Trie. */
     bool hasString(std::string query);
 
+    /* Used recursively to determine if a prefix of a string exists within the parent Trie. */
     bool hasPrefix(std::string prefix);
 
+    /* Used recursively to insert a word into the parent Trie. */
     void insert(std::string word);
 
   private:
 
+    /* Marks if this trie node is an entry in the Trie. */
     bool isEntry = false;
+    /* Contains all possible extensions of the word prefix up until this point. */
     std::map<char, TrieNode*> children;
 
+    /* Helper for safely returning the tail of a string. */
     std::string getTail(std::string word);
 
 };
